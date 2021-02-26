4c5b8678160f4836d0bf0ce213c3a55d2dfdd524
kotasugya <no.shaking.throne974zero@gmail.com>
2021-02-26 15:47:25 +0900

    CreateRouting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :companies
+      resources :reviews do
+        resources :enrollments
+        resources :evaluations
+      end
+    end
+  end
 end
