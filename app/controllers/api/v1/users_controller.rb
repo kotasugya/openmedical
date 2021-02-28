05f6df7979e63f6b2056ba7db774b0ef116b3c41
kotasugya <no.shaking.throne974zero@gmail.com>
2021-03-01 00:14:07 +0900

    CreateUserNew

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,12 +2,16 @@ module Api
   module V1
     class UsersController < ApplicationController
       def create
-        user = User.new(user_params)
-        if user.save
-          render json: user
+        @user = User.new(user_params)
+        if @user.save
+          render json: {user: @user}
         else
-          render json: user.errors
+          render json: {status: 500}
         end
+      end
+
+      def update
+        
       end
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+//components
+import {UsersNew} from './containers/users/UsersNew.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path = "/users/new">
+          <UsersNew />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
