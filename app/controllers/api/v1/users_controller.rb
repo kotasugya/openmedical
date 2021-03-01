997496ca696c071027c031f3fbf03c7b3008fc36
kotasugya <no.shaking.throne974zero@gmail.com>
2021-03-02 00:08:41 +0900

    SuccessApiPost

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,10 +4,16 @@ module Api
       def create
         @user = User.new(user_params)
         if @user.save
-          render json: {user: @user}
+          render json: {status: :created}
         else
-          render json: {status: 500}
+          render json: @user.errors, status: :unprocessable_entity
         end
+      end
+
+      def show
+      end
+
+      def index
       end
 
       def update
